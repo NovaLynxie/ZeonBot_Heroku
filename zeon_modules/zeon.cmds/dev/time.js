3d71a05d6c579155eb3cbe5ffd3fd40e18c6e622
Macky Fitz <pixelgamerx32@gmail.com>
2019-11-05 23:32:36 +0000

    Create time.js
    Adding a developer command to get local time and post to channel and console.

---
+++ zeon_modules/zeon.cmds/dev/time.js
@@ -0,0 +1,15 @@
+var timeHrs = new Date().getHours();
+var timeMins = new Date().getMinutes();
+var timeSecs = new Date().getSeconds();
+module.exports.run = (bot, message) => {
+    console.log("LocalTime: "+timeHrs+':'+timeMins+':'+timeSecs)
+    message.channel.send(timeHrs+':'+timeMins+':'+timeSecs) 
+};
+
+module.exports.help = {
+    name: "time",
+	aliases: ["t"],
+	description: "Responds with local time for bot.",
+	usage: "time",
+	category: "Developer",
+};
